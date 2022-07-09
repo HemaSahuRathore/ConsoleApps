a0d6970537970f59871fe641d84516e08157a3ed
Hema <hemasahu019@gmail.com>
2022-07-09 08:51:24 +0530

    Human Class create

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class HumanClass
+    {
+        public string name;
+        public int age;
+        public float height;
+
+        public void Walk()
+        {
+            Console.WriteLine("Human can walk");
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Name:" + name + " Age: " + age + " Height: " + height);
+            Console.ReadLine();
+        }
+
+
+        public HumanClass(string humanName, int humanAge, float humanHeight)
+        {
+            name = humanName;
+            age = humanAge;
+            height = humanHeight;
+
+        }
+
+    }
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HumanClass Riya = new HumanClass("Riya", 23, 5.2f);
+            HumanClass Piya = new HumanClass("Piya", 24, 5.1f);
+
+            Riya.DisplayDetails();
+            Piya.DisplayDetails();
+        }
+    }
+}   
